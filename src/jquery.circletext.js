eee0876c378772bc294883963a1c73c104ec5d70
Ben Major <ben.major88@gmail.com>
2017-10-11 15:15:37 +0100

    Create jquery.circletext.js

---
+++ src/jquery.circletext.js
@@ -0,0 +1,42 @@
+(function($) {
+
+    // Fit and stretch text to fit on a cirlce with given radius x
+    $.fn.circleText = function( settings )
+    {
+        // Preserve method chaining in jQuery:
+        return this.each(function() {
+
+            var text    = $(this).text().split(''),
+                degrees = 360 / text.length,
+                origin  = 0,
+                $this   = $(this);
+
+            // Clear the element so we can append it:
+            $(this).empty();
+
+            // Extend the options into opts:
+            var opts = $.extend({ radius: 10 }, settings);
+
+            // Loop through the characters:
+            text.forEach(function(char) {
+
+                // Wrap the char inside a <span>:
+                var $char = $('<span />', { text: char });
+
+                // Apply the required CSS to the letter:
+                $char.css({
+                    'height':           opts.radius,
+                    'position':         'absolute',
+                    'transform':        'rotate('+origin+'deg)',
+                    'transform-origin': '0 100%'
+                });
+
+                // Now append the character to the element:
+                $char.appendTo( $this );
+
+                // Finally, increment the origin for the next char by degrees:
+                origin+= degrees;
+            });
+        });
+    };
+} (jQuery));
